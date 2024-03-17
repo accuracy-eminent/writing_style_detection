7c54dc4a25317d54a656a26daebe53a54977cc6c
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2024-03-16 19:15:12 -0500

    Add initial streamlit, clean up structure, try varied length inputs to model

---
+++ src/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import utils
+
+st.title("Detection of writing style")
+
+st.write("")
+
+st.text_area()
---
+++ src/model.py
@@ -0,0 +1,10 @@
+class Model():
+    def __init__(self):
+        pass
+    def load_weights(self, file):
+        pass
+    def load_data(self, text):
+        pass
+    def predict(self):
+        pass
+
