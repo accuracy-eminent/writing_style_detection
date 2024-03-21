17f0384f1d16c1690f58b3f720ce9d1825052739
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2024-03-20 21:10:22 -0500

    Start code for classifiers

--- src/model.py
+++ src/model.py
@@ -1,5 +1,9 @@
+from sklearn.ensemble import GradientBoostingClassifier
+
 class Model():
     def __init__(self):
+        pass
+    def train(self, training_data):
         pass
     def load_weights(self, file):
         pass
@@ -8,3 +12,31 @@ class Model():
     def predict(self):
         pass
 
+class TabularModel(Model):
+    def __init__(self):
+        self.model = GradientBoostingClassifier()
+    def load_weights(self, file):
+        self.model = pickle.load(file)
+    def load_data(self, data):
+        self.data = utils.get_data_df([data], ['unknown'])
+    def predict(self):
+        y_pred = self.model.predict_proba(self.data)
+        return y_pred
+
+class NNModel(Model):
+    def __init__(self):
+        # TODO: define vocab
+        # self.vocab = load vocab from file
+        embedding_dim = 64
+        hidden_size = 128
+        num_classes = 3
+        vocab_size = 1000
+        self.model = AuthorClassifier(vocab_size, embedding_dim, hidden_size, num_classes)
+    def load_weights(self):
+        pass
+    def load_data(self, data):
+        self.data = TextDataset(data, self.vocab)
+        pass
+    def predict(self):
+        y_pred = model(self.data)
+        return y_pred
