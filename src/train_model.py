e42bdf16115a3e9f11f4b3ac3e17161a68be1d1f
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2024-03-21 20:57:17 -0500

    Add more standardized model training

--- src/train_model.py
+++ src/train_model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import utils
 import model as md
+from sklearn import metrics
+import pickle
 
 # %%
 # Load in datasets
@@ -41,7 +43,7 @@ train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 # %%
 # Training loop
-num_epochs = 1
+num_epochs = 10
 for epoch in range(num_epochs):
     print(epoch)
     nn_model.train()
@@ -54,4 +56,56 @@ for epoch in range(num_epochs):
         loss_vals.append(loss.item())
         optimizer.step()
     print(np.mean(loss_vals))
+# %%
+# Evaluation
+nn_model.eval()
+total_correct = 0
+total_samples = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        outputs = nn_model(inputs)
+        _, predicted = torch.max(outputs, 1)
+        total_samples += labels.size(0)
+        total_correct += (predicted == labels).sum().item()
+
+accuracy = total_correct / total_samples
+print(f"Test Accuracy: {accuracy * 100:.2f}%")
+
+# %%
+# Save model weights
+torch.save(nn_model.state_dict, "../data/nn.pth")
+
+
+# %%
+# Tabular model
+# Load in data
+# Do feature engineering
+# Use ngram frequency as features
+# cd_1grams is the frequency of 1-grams associated with Charles Dickens, for example
+data_df_train = utils.get_data_df(book_samples_train, utils.book_authors_train)
+data_df_test = utils.get_data_df(book_samples_test, utils.book_authors_test)
+
+# %%
+# Prepare datasets for training
+tgt_cols = data_df_test.columns
+tgt_cols = ['author_name']
+X_train = data_df_train.drop(tgt_cols,axis=1)
+y_train = data_df_train.filter(tgt_cols).to_numpy().ravel()
+X_test = data_df_test.drop(tgt_cols,axis=1)
+y_test = data_df_test.filter(tgt_cols).to_numpy().ravel()
+
+# %%
+model_tab = md.TabularModel()
+tb_model = model_tab.get_raw_model()
+# %%
+tb_model.fit(X_train, y_train)
+y_pred = tb_model.predict(X_test)
+acc = metrics.accuracy_score(y_test, y_pred)
+print("Accuracy: ", acc)
+
+# %%
+# Save model
+with open( "../data/tabular.pkl", "wb") as file:
+    pickle.dump(tb_model, file)
 # %%
